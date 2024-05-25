7762a93e0df78a4fd23cb0ffbcd5c924d72363ea
iammahesh123 <maheshkadambala18@gmail.com>
2024-05-25 15:39:45 +0530

    Initial commit

---
+++ src/main/java/org/example/securingrestfulapi/SecuringResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.securingrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringResTfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuringResTfulApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/securingrestfulapi/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.example.securingrestfulapi.service;
+
+
+import org.example.securingrestfulapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.example.securingrestfulapi.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
+
